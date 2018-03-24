2df219a8d2a807317abdf79de218ba86ac27aa48
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-03-23 21:49:11 -0400

    Added addition error handling for does not exist user

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_hr_data(user_email):
 
     try:
         user = models.User.objects.raw({'_id': user_email}).first()
-    except models.DoesNotExist:
+    except DoesNotExist:
         logging.exception('User email does not exist')
         return 'User does not exist', 400
 
@@ -66,7 +66,7 @@ def get_avg_hr(user_email):
 
     try:
         user = models.User.objects.raw({'_id': user_email}).first()
-    except models.DoesNotExist:
+    except DoesNotExist:
         logging.exception('User email does not exist')
         return 'User does not exist', 400
 
@@ -88,7 +88,13 @@ def get_interval_avg_hr():
         return 'Incorrectly formatted JSON', 400
 
     email = json['user_email']
-    user = models.User.objects.raw({'_id': email}).first()
+
+    try:
+        user = models.User.objects.raw({'_id': email}).first()
+    except DoesNotExist:
+        logging.exception('User does not exist')
+        return 'User does not exist', 400
+
     age = user.age
     heart_rate_list = user.heart_rate
     time_list = user.heart_rate_times
